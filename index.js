2d910af505a4af722dd1985b48cc118ac140a82d
ericwm76 <eric.meldrum@gmail.com>
2020-04-19 18:07:08 -0600

    Create calculations for totalInterval, numberOfIntervals, distancePerInterval, partialIntervalLength, and totalDistance

---
+++ index.js
@@ -0,0 +1,21 @@
+const stats = {
+  Alfie: {speed: 5, runningInterval: 17, restingInterval: 57},
+  Lincoln: {speed: 28, runningInterval: 6, restingInterval: 133},
+  Tuck: {speed: 23, runningInterval: 4, restingInterval: 83},
+  Sullivan: {speed: 19, runningInterval: 7, restingInterval: 108},
+  Stitch: {speed: 4, runningInterval: 22, restingInterval: 62},
+  Zema: {speed: 16, runningInterval: 6, restingInterval: 86},
+  Mara: {speed: 27, runningInterval: 6, restingInterval: 135},
+  Butter: {speed: 12, runningInterval: 6, restingInterval: 53},
+  Ember: {speed: 15, runningInterval: 4, restingInterval: 50},
+};
+
+const calculateDistanceTraveled = (pupName, seconds) => {
+  const totalInterval = (stats[pupName].runningInterval + stats[pupName].restingInterval);
+  const numberOfCompleteIntervals = Math.floor(seconds / totalInterval);
+  const distancePerInterval = (stats[pupName].speed * stats[pupName].runningInterval);
+  const partialIntervalLength = seconds - (totalInterval * numberOfCompleteIntervals);
+  const totalDistance = (numberOfCompleteIntervals * distancePerInterval)
+}
+
+calculateDistanceTraveled('Alfie', 3641)
